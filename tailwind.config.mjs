7840e400dbf872f7c1cb9d0bc0d49f755da271a5
hru <hru@hrus-Mac-mini.fritz.box>
2023-12-02 11:50:48 +0100

    add theme toggler

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -3,6 +3,9 @@ export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
 		extend: {},
+		colors: {
+			light: "hsl(33,16%,87%)",
+		}
 	},
 	plugins: [],
 }
